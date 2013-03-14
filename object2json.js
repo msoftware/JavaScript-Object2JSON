b94c46e306d424a4539c7dc36f37f9cfaf96edb9
unknown <Michael@.(none)>
2013-03-14 21:51:04 +0100

    Init

---
+++ object2json.js
@@ -0,0 +1,27 @@
+
+var object2json = function(obj) {
+	var t = typeof (obj);
+	if (t != "object" || obj === null) {
+		if (t == "string")
+			obj = '"' + obj + '"';
+		return String(obj);
+	} else {
+		var n, v, json = [], arr = (obj && obj.constructor == Array);
+
+		for (n in obj) {
+			v = obj[n];
+			t = typeof (v);
+			if (t == "string") {
+
+				v = '"' + v + '"';
+			} else {
+				if (t == "object" && v !== null) {
+					v = object2json(v);
+				}
+			}
+			json.push(( arr ? '' : '"' + n + '":') + String(v));
+		}
+
+		return ( arr ? "[" : "{") + String(json) + ( arr ? "]" : "}");
+	}
+}
